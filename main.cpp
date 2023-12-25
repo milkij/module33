2db7387a106191e0ddc7cb7502acbaf9e05d3b10
shirokov.mos@ya.ru <shirokov.mos@ya.ru>
2023-12-25 13:30:02 +0300

    fix task 3

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@
 
 using namespace std;
 template<typename T1, typename T2>
-struct DATA
+class DATA
 {
+public:
     T1 id;
     T2 value;
-    DATA(){};
     DATA(T1 _id, T2 _value):id(_id),value(_value){};
-    DATA(T1 _id):id(_id) {value=" ";};
+    DATA(T1 _id):id(_id) {};
+    friend bool operator==(const DATA &lhs, const DATA &rhs)
+    {
+        if(lhs.id==rhs.id) return true;
+        else return false;
+    }
 };
 
 template<typename T1, typename T2>
@@ -23,65 +28,28 @@ public:
 
     auto add (const T1 _id, const T2 _value)
     {
-        std::cout << "\tADD" << std::endl;
-        DATA<T1,T2> obj(_id,_value);
-        reg.emplace_back(obj);
+        reg.push_back(DATA<T1,T2>(_id,_value));
     };
-    auto findV(const T1 _id)
-    {
-        std::cout << "\tFIND" << std::endl;
-        bool isFind=false;
-        for(auto i=0; i<reg.size(); ++i)
-        {
-            if(reg[i].id ==_id)
-            {
-                isFind= true;
-                std::cout << reg[i].id << " | " << reg[i].value << std::endl;
-            }
-        }
-        if(!isFind)
-        {
-            std::cout << "No data found" << std::endl;
-        }
-    }
 
-    DATA<T1,T2> find (const T1 _id)
+    void find (const T1 _id)
     {
-
         for (auto it : reg)
         {
             if(it.id==_id)
             {
-                DATA<T1,T2> obj;
-                obj.id=it.id;
-                obj.value=it.value;
-                return obj;
+                cout << it.id << " | " << it.value << endl;
             }
         }
     }
 
-    auto remove (T1 _id)
+    auto remove (DATA<T1, T2> _id)
     {
         std::cout << "\tREMOVE" << std::endl;
-        auto it = std::remove(reg.begin(), reg.end(), DATA<T1, T2>(_id));
+        auto it = std::remove(reg.begin(), reg.end(), _id);
         reg.erase(it, reg.end());
-        /*while (true) {
-            //Не понимаю почему это не работает. Как это пофиксить?
-            DATA<T1,T2> obj(_id);
-            //auto it = std::find(reg.begin(), reg.end(), obj);
-            //auto it = std::find(reg.begin(), reg.end(), [_id] (DATA<T1,T2> obj) {obj.id=_id; return obj;});
-            //auto it = std::find(reg.begin(), reg.end(), find(_id));
-            auto it = std::find(reg.begin(), reg.end(), DATA<T1,T2>(_id,""));
-            if (it != reg.end()) {
-                reg.erase(it);
-            } else {
-                break;
-            }
-        }*/
     };
     auto print ()
     {
-        std::cout << "\tPRINT" << std::endl;
         for (auto i : reg)
         {
             std::cout << i.id << " | " << i.value << std::endl;
@@ -91,15 +59,24 @@ public:
 
 };
 
+void print(string text="-----------")
+{
+    cout << text << endl;
+}
+
 int main() {
     REGISTRY<int, std::string> registr;
-    registr.add(1,"str");
-    registr.add(1,"str2");
-    registr.add(3,"str3");
-    registr.add(2,"str3");
-    registr.add(3,"str3");
+    print("ADD");
+    registr.add(1,"Ivan");
+    registr.add(4,"Petr");
+    registr.add(3,"Sidr");
+    registr.add(4,"Serg");
     registr.print();
-    registr.findV(1);
-    registr.remove(3);
+
+    print("REMOVE");
+    registr.remove(DATA<int, std::string>(4));
+
+    print("PRINT");
     registr.print();
+
 }
